fd34c1929ec66b6b93fb7451328d2724e31f7a43
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-03-26 08:52:33 +0100

    init

---
+++ resources/views/livewire/pages/welcome-page.blade.php
@@ -0,0 +1,9 @@
+<?php
+use function Livewire\Volt\{state, layout};
+
+layout('layouts.guest');
+?>
+
+<div>
+    This is my test
+</div>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Volt::route('/', 'pages.welcome-page')->name('welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__ . '/auth.php';
