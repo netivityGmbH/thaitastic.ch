2f3b3e4ec48850add487f49296ec3704cca878e9
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-03-28 23:25:00 +0100

    Update welcome section layout and add logo

--- resources/views/components/partials/welcome-section.blade.php
+++ resources/views/components/partials/welcome-section.blade.php
@@ -1,18 +1,21 @@
     <section id="welcome" class="min-h-dvh min-w-screen">
-        <div class="relative z-10 flex flex-col items-center justify-center px-4 pt-24">
+        <div class="relative z-10 flex flex-col items-center justify-center px-4 pt-12">
+
+            <img src="/media/logo.svg" alt="Logo" class="w-56">
+
             <h1 class="mb-4 text-center font-serif text-6xl text-primary">ThaiTastic Bites</h1>
             <p class="font-serif uppercase text-primary md:text-xl">Dein Ticket für einen Geschmacks-Rave</p>
         </div>
 
         <div class="absolute bottom-4 left-0 right-0 z-10 flex justify-center">
-
             <a href="#contact">
                 <img src="/media/scroll.gif" alt="Scroll"
                     class="size-16 transition-all duration-300 ease-in-out hover:-translate-y-1"></a>
 
         </div>
 
-        <div class="absolute bottom-0 left-0 right-0 overflow-hidden">
-            <img src="/media/background.png" alt="Background" class="h-dvh object-none sm:h-auto sm:object-fill">
+        <div class="absolute bottom-0 left-0 right-0 overflow-x-hidden">
+            <img src="/media/background.png" alt="Background"
+                class="h-dvh object-none brightness-50 sm:h-auto sm:object-fill">
         </div>
     </section>
