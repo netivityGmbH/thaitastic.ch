754aa7a46732cb7a9b15096919df1bf6491b96d1
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-03-28 23:25:13 +0100

    Add menu section to index page

---
+++ resources/views/components/partials/menu-section.blade.php
@@ -0,0 +1,10 @@
+    <section id="contact" class="min-h-dvh min-w-screen relative relative py-12">
+        <div class="relative z-10 flex flex-col items-center justify-center px-4 pt-12 font-serif text-primary">
+            <h2 class="mb-4 text-center text-5xl">Menu</h2>
+            <p class="md:text-xl">Köstlichkeiten aus dem Herzen Thailands. Entdecke unser Menü!</p>
+        </div>
+
+        <div class="relative z-10 flex justify-center pt-12">
+
+        </div>
+    </section>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,7 @@
 <x-guest-layout>
     <x-partials.welcome-section />
 
+    <x-partials.menu-section />
+
     <x-partials.contact-section />
 </x-guest-layout>
