45802d26e95ebd6c5a4930deeaf71405468f5f0b
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-03-29 17:33:56 +0100

    Add fade-in animation to contact and menu sections

--- resources/views/components/partials/contact-section.blade.php
+++ resources/views/components/partials/contact-section.blade.php
@@ -1,10 +1,12 @@
-    <section id="contact" class="min-h-dvh min-w-screen relative relative py-12">
-        <div class="relative z-10 flex flex-col items-center justify-center px-4 pt-12 font-serif text-primary">
-            <h2 class="mb-4 text-center text-5xl">Kontakt</h2>
-            <p class="md:text-xl">Hast du Fragen oder Anregungen? Dann schreib uns!</p>
-        </div>
+<section id="contact" class="min-h-dvh min-w-screen relative py-12">
+    <div class="relative z-10 flex flex-col items-center justify-center px-4 pt-12 font-serif text-primary">
+        <h2 class="mb-4 text-center text-5xl">Kontakt</h2>
+        <p class="md:text-xl">Hast du Fragen oder Anregungen? Dann schreib uns!</p>
+    </div>
 
-        <div class="relative z-10 flex justify-center pt-12">
+    <div class="relative z-10 flex justify-center pt-12" x-data="{ show: false }" x-intersect.once="show = true">
+        <div :class="{ 'fade-in': show }">
             <livewire:contact-form />
         </div>
-    </section>
+    </div>
+</section>
--- resources/views/components/partials/menu-section.blade.php
+++ resources/views/components/partials/menu-section.blade.php
@@ -1,13 +1,12 @@
-    <section id="menu" class="min-w-screen relative relative py-12">
-        <div class="relative z-10 flex flex-col items-center justify-center px-4 pt-12 font-serif text-primary">
-            <h2 class="mb-4 text-center text-5xl">Menu</h2>
-            <p class="md:text-xl">Köstlichkeiten aus dem Herzen Thailands. Entdecke unser Menü!</p>
-        </div>
+<section id="menu" class="min-w-screen relative py-12">
+    <div class="relative z-10 flex flex-col items-center justify-center px-4 pt-12 font-serif text-primary">
+        <h2 class="mb-4 text-center text-5xl">Menu</h2>
+        <p class="md:text-xl">Köstlichkeiten aus dem Herzen Thailands. Entdecke unser Menü!</p>
+    </div>
 
-        <div class="relative z-10 flex justify-center pt-12">
-            <div class="max-w-xl flex-1">
-                <livewire:menu-list />
-            </div>
-
+    <div class="relative z-10 flex justify-center pt-12" x-data="{ show: false }" x-intersect.once="show = true">
+        <div class="max-w-xl flex-1" :class="{ 'fade-in': show }">
+            <livewire:menu-list />
         </div>
-    </section>
+    </div>
+</section>
