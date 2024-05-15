c191eb246abe13d27f9424e28423a63b7e1f4ff9
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-05-15 08:52:11 +0200

    added social media

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -3,5 +3,11 @@
         Designed and Published by
         <a href="https://www.netivity.ch/" target="_blank" class="text-secondary">netivity GmbH</a>
     </p>
-    <a href="{{ route('impressum') }}" wire:navigate.hover class="text-center">Impressum</a>
+    <p class="flex justify-center gap-4">
+        <a href="https://www.facebook.com/thaitasticbites" target="_blank" class="text-center">Facebook</a>
+        <a href="https://www.instagram.com/thaitasticbites" target="_blank" class="text-center">Instagram</a>
+        <a href="{{ route('impressum') }}" wire:navigate.hover class="text-center">Impressum</a>
+    </p>
+
+
 </div>
